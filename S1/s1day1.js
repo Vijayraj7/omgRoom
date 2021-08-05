6621c8690627e43c0377c54f9d367cb4314c044e
Vijayraj7 <66467300+Vijayraj7@users.noreply.github.com>
2021-08-05 22:51:56 +0530

    Add files via upload

---
+++ S1/s1day1.js
@@ -0,0 +1,222 @@
+var s1resultsday1 =
+[
+    //3     //1
+    {
+        "clan": 'CBA',
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "8",
+        "KILLPOINTS": "6",
+        "TOTAL": "14"
+    },
+    //2
+    {
+        "clan": "KANDAM",
+        "MatchesPlayed": "0",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+    //15   //3
+    {
+        "clan": "teamRGesports",
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+    //2  //4
+
+    {
+        "clan": "PIRATES",
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "9",
+        "KILLPOINTS": "10",
+        "TOTAL": "19"
+    },
+    //9    //5
+    {
+        "clan": "Dolphin",
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "2",
+        "KILLPOINTS": "2",
+        "TOTAL": "4"
+    },
+    //6  //6
+    {
+        "clan": "AGENT66",
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "5",
+        "KILLPOINTS": "8",
+        "TOTAL": "13"
+    },
+    //7
+    {
+        "clan": "TEAM BRS",
+        "MatchesPlayed": "0",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+    //8
+    {
+        "clan": "VORTEX ESPORTS",
+        "MatchesPlayed": "0",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+    //1    //10
+    {
+        "clan": "BEAST 12 OFFICIAL",
+        "MatchesPlayed": "1",
+        "WIN": "1",
+        "PLACEPOINTS": "10",
+        "KILLPOINTS": "36",
+        "TOTAL": "46"
+    },
+    //16     //11
+    {
+        "clan": "MALLU PIRATES",
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+    //5      //12
+    {
+        "clan": "Lord of guns",
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "6",
+        "KILLPOINTS": "8",
+        "TOTAL": "14"
+    },
+    //13
+    {
+        "clan": "HYD OFFICIALS",
+        "MatchesPlayed": "0",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+    //14
+    {
+        "clan": "Saint eSports",
+        "MatchesPlayed": "0",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+    //15
+    {
+        "clan": "FORCE X",
+        "MatchesPlayed": "0",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+    //6  //16
+    {
+        "clan": "Los Zetas",
+        "MatchesPlayed": "0",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+    //7    //17
+    {
+        "clan": "WZ",
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "4",
+        "KILLPOINTS": "2",
+        "TOTAL": "6"
+    },
+    //18
+    {
+        "clan": "Zealous",
+        "MatchesPlayed": "0",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+    //19
+    {
+        "clan": "SaintN",
+        "MatchesPlayed": "0",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+    //14   //20
+    {
+        "clan": "zyko",
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "2",
+        "TOTAL": "2"
+    },
+    //12  //21
+    {
+        "clan": "RAGE",
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+    //10   //22
+    {
+        "clan": "NOISE",
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "1",
+        "KILLPOINTS": "0",
+        "TOTAL": "1"
+    },
+    //8  //23
+    {
+        "clan": "DMEN",
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "3",
+        "KILLPOINTS": "10",
+        "TOTAL": "13"
+    },
+    //4  //24
+    {
+        "clan": "OMG",
+        "MatchesPlayed": "1",
+        "WIN": "0",
+        "PLACEPOINTS": "9",
+        "KILLPOINTS": "6",
+        "TOTAL": "15"
+    },
+    //25
+    {
+        "clan": "OMG 2",
+        "MatchesPlayed": "0",
+        "WIN": "0",
+        "PLACEPOINTS": "0",
+        "KILLPOINTS": "0",
+        "TOTAL": "0"
+    },
+
+];
+
